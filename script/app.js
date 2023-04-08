8d23c749cb115863cb897535b2cd1350e8ae04f1
Aamna <rafiqpatni1220@gmail.com>
2023-04-08 22:36:18 +0500

    first commit

---
+++ script/app.js
@@ -0,0 +1,55 @@
+//Day 1 chapter 1-9 date 6-4-23
+//alert("Thanks for your input")
+//var username="Aamna";
+//username="Rafiq"
+//var nationality;
+//nationality="u.s";
+//var floogle="mark";
+//var name="mark";
+//var lessonauthor= Mark;
+//var x="Mark"
+//var weight=100;
+//weight+25;
+//var originalnum=27;
+//var newnum=originalnum+5;
+//var num=23;
+//var Tobeadded=7;
+//var newnum= num + Tobeadded;
+//var realnum=22;
+//realnum=realnum+10
+//var popularNumber = 4; //But you can also write this. 
+//var popularNumber = 2 + 2;// You can also write: 
+//alert(2 + 2);
+//var popularNumber = 12 - 24; //This one assigns the product of 3 times 12, 36, to the variable. 
+//var popularNumber = 3 * 12;
+//var num = 10;  
+//var popularNumber = num + 200; //You can also use nothing but variables. 
+//var num = 10;  
+//var anotherNum = 1; 
+//var popularNumber = num + anotherNum; 
+//var num = 1; 2 var newNum = ++num; 
+//var num = 1; 2 var newNum = num--;// num is decremented to 0, and newNum gets the original value of num, 1. But if you put the minuses before the variable, newNum is assigned the decremented, not the original, value. Both num and newNum wind up with a value of 0. 
+//var num = 1;
+//var newNum = --num; 
+//var totalCost = 1 + 3 * 4; 
+//var totalCost = (1 + 3) * 4; //Here's another example. 
+//var resultOfComputation = (2 * 4) * 4 + 2; 
+//alert("Thanks, " + userName + "!"); 
+//var message = "Thanks, "; 
+//var banger = "!"; 
+//alert(message + userName + banger); 
+//var message = "Thanks, "; 
+//var userName = "Susan"; 
+//var banger = "!"; 
+//var customMess = message + userName + banger; 
+//alert(customMess);
+//alert("2" + "2");
+//alert("2 plus 2 equals " + 2 + 2);
+//var spec = prompt("Your species?", "human"); 
+//var question = "Your species?"; 
+//var defaultAnswer = "human";
+//var spec = prompt(question, defaultAnswer); 
+var numberOfCats = prompt("How many cats?"); 
+var tooManyCats = numberOfCats + 1;
+
+
